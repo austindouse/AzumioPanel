e6b1cbd8fc3dca64ca55b4d19b210de5a1b87124
Austin Douse <douse.austin@gmail.com>
2020-06-23 17:02:59 -0400

    working out one bug

--- client/dataStore.js
+++ client/dataStore.js
@@ -1,6 +1,6 @@
 const exampleData = {
   question: "What are your main areas of focus?",
-  prompt: "This will help us build a personalized experience for you.",
+  prompt: "This will help us build a personalized experience for you",
   options: [
     "Blood Glucose",
     "Medication",
